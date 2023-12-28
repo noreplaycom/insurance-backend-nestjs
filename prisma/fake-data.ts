e921284f8d552fb5124aebe024c6a782738a2b7b
Dikki Setiawan <initialsds@gmail.com>
2023-12-28 21:59:06 +0700

    fix fake-data

--- prisma/fake-data.ts
+++ prisma/fake-data.ts
@@ -1,7 +1,18 @@
-import { Permission, ParticipantStatus, Position, Class, ClaimChannel, DocumentSource, DocumentType, Color, ApplicationType, AdmedicaStatus, Gender, ClaimStatusType, TransactionType } from '@prisma/client';
+import {
+  Permission,
+  ParticipantStatus,
+  Position,
+  Class,
+  ClaimChannel,
+  DocumentSource,
+  DocumentType,
+  Color,
+  ApplicationType,
+  AdmedicaStatus,
+  Gender,
+  ClaimStatusType,
+} from '@prisma/client';
 import { faker } from '@faker-js/faker';
-
-
 
 export function fakeUser() {
   return {
@@ -32,7 +43,21 @@ export function fakeRole() {
   return {
     name: faker.name.fullName(),
     description: undefined,
-    permissions: faker.helpers.arrayElement([Permission.CREATE_CLAIM, Permission.UPDATE_CLAIM, Permission.UPDATE_CLAIM_STATUS, Permission.UPDATE_STATUS_FINANCIAL, Permission.RELEASE_CLAIM, Permission.DELETE_CLAIM_DOCUMENT, Permission.CREATE_CLAIM_DOCUMENT, Permission.IMPORT_CLAIM, Permission.EXPORT_CLAIM, Permission.CREATE_PARTICIPANT, Permission.UPDATE_PARTICIPANT, Permission.IMPORT_PARTICIPANT, Permission.EXPORT_PARTICIPANT] as const),
+    permissions: faker.helpers.arrayElement([
+      Permission.CREATE_CLAIM,
+      Permission.UPDATE_CLAIM,
+      Permission.UPDATE_CLAIM_STATUS,
+      Permission.UPDATE_STATUS_FINANCIAL,
+      Permission.RELEASE_CLAIM,
+      Permission.DELETE_CLAIM_DOCUMENT,
+      Permission.CREATE_CLAIM_DOCUMENT,
+      Permission.IMPORT_CLAIM,
+      Permission.EXPORT_CLAIM,
+      Permission.CREATE_PARTICIPANT,
+      Permission.UPDATE_PARTICIPANT,
+      Permission.IMPORT_PARTICIPANT,
+      Permission.EXPORT_PARTICIPANT,
+    ] as const),
   };
 }
 export function fakeRoleComplete() {
@@ -41,30 +66,62 @@ export function fakeRoleComplete() {
     name: faker.name.fullName(),
     description: undefined,
     higherLevelThanId: undefined,
-    permissions: faker.helpers.arrayElement([Permission.CREATE_CLAIM, Permission.UPDATE_CLAIM, Permission.UPDATE_CLAIM_STATUS, Permission.UPDATE_STATUS_FINANCIAL, Permission.RELEASE_CLAIM, Permission.DELETE_CLAIM_DOCUMENT, Permission.CREATE_CLAIM_DOCUMENT, Permission.IMPORT_CLAIM, Permission.EXPORT_CLAIM, Permission.CREATE_PARTICIPANT, Permission.UPDATE_PARTICIPANT, Permission.IMPORT_PARTICIPANT, Permission.EXPORT_PARTICIPANT] as const),
+    permissions: faker.helpers.arrayElement([
+      Permission.CREATE_CLAIM,
+      Permission.UPDATE_CLAIM,
+      Permission.UPDATE_CLAIM_STATUS,
+      Permission.UPDATE_STATUS_FINANCIAL,
+      Permission.RELEASE_CLAIM,
+      Permission.DELETE_CLAIM_DOCUMENT,
+      Permission.CREATE_CLAIM_DOCUMENT,
+      Permission.IMPORT_CLAIM,
+      Permission.EXPORT_CLAIM,
+      Permission.CREATE_PARTICIPANT,
+      Permission.UPDATE_PARTICIPANT,
+      Permission.IMPORT_PARTICIPANT,
+      Permission.EXPORT_PARTICIPANT,
+    ] as const),
   };
 }
 export function fakeParticipant() {
   return {
-    gender: faker.helpers.arrayElement([Gender.MALE, Gender.FEMALE, Gender.UNKNOWN] as const),
+    gender: faker.helpers.arrayElement([
+      Gender.MALE,
+      Gender.FEMALE,
+      Gender.UNKNOWN,
+    ] as const),
     birthDate: faker.datatype.datetime(),
     nationalId: faker.lorem.words(5),
     familyCardNumber: faker.lorem.words(5),
     taxId: undefined,
     isActive: faker.datatype.boolean(),
-    status: faker.helpers.arrayElement([ParticipantStatus.PENSIUNAN, ParticipantStatus.PASANGAN, ParticipantStatus.YATIM, ParticipantStatus.JANDADUDA6BULAN] as const),
+    status: faker.helpers.arrayElement([
+      ParticipantStatus.PENSIUNAN,
+      ParticipantStatus.PASANGAN,
+      ParticipantStatus.YATIM,
+      ParticipantStatus.JANDADUDA6BULAN,
+    ] as const),
     bankAccountId: faker.datatype.number(),
   };
 }
 export function fakeParticipantComplete() {
   return {
-    gender: faker.helpers.arrayElement([Gender.MALE, Gender.FEMALE, Gender.UNKNOWN] as const),
+    gender: faker.helpers.arrayElement([
+      Gender.MALE,
+      Gender.FEMALE,
+      Gender.UNKNOWN,
+    ] as const),
     birthDate: faker.datatype.datetime(),
     nationalId: faker.lorem.words(5),
     familyCardNumber: faker.lorem.words(5),
     taxId: undefined,
     isActive: faker.datatype.boolean(),
-    status: faker.helpers.arrayElement([ParticipantStatus.PENSIUNAN, ParticipantStatus.PASANGAN, ParticipantStatus.YATIM, ParticipantStatus.JANDADUDA6BULAN] as const),
+    status: faker.helpers.arrayElement([
+      ParticipantStatus.PENSIUNAN,
+      ParticipantStatus.PASANGAN,
+      ParticipantStatus.YATIM,
+      ParticipantStatus.JANDADUDA6BULAN,
+    ] as const),
     userId: faker.datatype.uuid(),
     relationId: undefined,
     bankAccountId: faker.datatype.number(),
@@ -132,13 +189,35 @@ export function fakeBankAccountComplete() {
 }
 export function fakeEmployment() {
   return {
-    employmentPosition: faker.helpers.arrayElement([Position.AMGR, Position.AVP, Position.PGD, Position.MGR, Position.SASST, Position.VP, Position.SAVP, Position.SMGR, Position.SAMGR, Position.ASST] as const),
+    employmentPosition: faker.helpers.arrayElement([
+      Position.AMGR,
+      Position.AVP,
+      Position.PGD,
+      Position.MGR,
+      Position.SASST,
+      Position.VP,
+      Position.SAVP,
+      Position.SMGR,
+      Position.SAMGR,
+      Position.ASST,
+    ] as const),
   };
 }
 export function fakeEmploymentComplete() {
   return {
     id: faker.datatype.number(),
-    employmentPosition: faker.helpers.arrayElement([Position.AMGR, Position.AVP, Position.PGD, Position.MGR, Position.SASST, Position.VP, Position.SAVP, Position.SMGR, Position.SAMGR, Position.ASST] as const),
+    employmentPosition: faker.helpers.arrayElement([
+      Position.AMGR,
+      Position.AVP,
+      Position.PGD,
+      Position.MGR,
+      Position.SASST,
+      Position.VP,
+      Position.SAVP,
+      Position.SMGR,
+      Position.SAMGR,
+      Position.ASST,
+    ] as const),
     participantId: faker.datatype.uuid(),
     branchId: faker.datatype.number(),
     groupId: faker.datatype.number(),
@@ -209,7 +288,14 @@ export function fakeProgramParticipationComplete() {
 }
 export function fakeProgram() {
   return {
-    plan: faker.helpers.arrayElement([ApplicationType.SANTUNAN_HARIAN_RAWAT_INAP, ApplicationType.BANTUAN_BIAYA_CUCI_DARAH, ApplicationType.BANTUAN_KURSI_RODA, ApplicationType.BANTUAN_WALKER, ApplicationType.BANTUAN_IGD_UGD, ApplicationType.BANTUAN_RAWAT_INAP] as const),
+    plan: faker.helpers.arrayElement([
+      ApplicationType.SANTUNAN_HARIAN_RAWAT_INAP,
+      ApplicationType.BANTUAN_BIAYA_CUCI_DARAH,
+      ApplicationType.BANTUAN_KURSI_RODA,
+      ApplicationType.BANTUAN_WALKER,
+      ApplicationType.BANTUAN_IGD_UGD,
+      ApplicationType.BANTUAN_RAWAT_INAP,
+    ] as const),
     updatedAt: faker.datatype.datetime(),
     deletedAt: undefined,
     allowanceCeiling: faker.datatype.float(),
@@ -220,7 +306,14 @@ export function fakeProgram() {
 export function fakeProgramComplete() {
   return {
     id: faker.datatype.number(),
-    plan: faker.helpers.arrayElement([ApplicationType.SANTUNAN_HARIAN_RAWAT_INAP, ApplicationType.BANTUAN_BIAYA_CUCI_DARAH, ApplicationType.BANTUAN_KURSI_RODA, ApplicationType.BANTUAN_WALKER, ApplicationType.BANTUAN_IGD_UGD, ApplicationType.BANTUAN_RAWAT_INAP] as const),
+    plan: faker.helpers.arrayElement([
+      ApplicationType.SANTUNAN_HARIAN_RAWAT_INAP,
+      ApplicationType.BANTUAN_BIAYA_CUCI_DARAH,
+      ApplicationType.BANTUAN_KURSI_RODA,
+      ApplicationType.BANTUAN_WALKER,
+      ApplicationType.BANTUAN_IGD_UGD,
+      ApplicationType.BANTUAN_RAWAT_INAP,
+    ] as const),
     createdAt: new Date(),
     updatedAt: faker.datatype.datetime(),
     deletedAt: undefined,
@@ -233,8 +326,15 @@ export function fakeClaim() {
   return {
     updatedAt: faker.datatype.datetime(),
     deletedAt: undefined,
-    channel: faker.helpers.arrayElement([ClaimChannel.REGULER, ClaimChannel.WA, ClaimChannel.EMAIL] as const),
-    admedicaStatus: faker.helpers.arrayElement([AdmedicaStatus.NON_ADMEDICA, AdmedicaStatus.ADMEDICA] as const),
+    channel: faker.helpers.arrayElement([
+      ClaimChannel.REGULER,
+      ClaimChannel.WA,
+      ClaimChannel.EMAIL,
+    ] as const),
+    admedicaStatus: faker.helpers.arrayElement([
+      AdmedicaStatus.NON_ADMEDICA,
+      AdmedicaStatus.ADMEDICA,
+    ] as const),
   };
 }
 export function fakeClaimComplete() {
@@ -243,8 +343,15 @@ export function fakeClaimComplete() {
     createdAt: new Date(),
     updatedAt: faker.datatype.datetime(),
     deletedAt: undefined,
-    channel: faker.helpers.arrayElement([ClaimChannel.REGULER, ClaimChannel.WA, ClaimChannel.EMAIL] as const),
-    admedicaStatus: faker.helpers.arrayElement([AdmedicaStatus.NON_ADMEDICA, AdmedicaStatus.ADMEDICA] as const),
+    channel: faker.helpers.arrayElement([
+      ClaimChannel.REGULER,
+      ClaimChannel.WA,
+      ClaimChannel.EMAIL,
+    ] as const),
+    admedicaStatus: faker.helpers.arrayElement([
+      AdmedicaStatus.NON_ADMEDICA,
+      AdmedicaStatus.ADMEDICA,
+    ] as const),
     companyId: faker.datatype.number(),
     participantId: faker.datatype.uuid(),
     claimFinancialId: faker.datatype.number(),
@@ -262,8 +369,19 @@ export function fakeDocument() {
     size: faker.datatype.number(),
     printCount: faker.datatype.number(),
     updatedAt: faker.datatype.datetime(),
-    source: faker.helpers.arrayElement([DocumentSource.UPLOADED, DocumentSource.GENERATED] as const),
-    type: faker.helpers.arrayElement([DocumentType.INVOICE, DocumentType.CLAIM_FORM, DocumentType.REJECTION_LETTER, DocumentType.GUARANTEE_LETTER, DocumentType.RECAP, DocumentType.EXPEDITION, DocumentType.TRANSFER] as const),
+    source: faker.helpers.arrayElement([
+      DocumentSource.UPLOADED,
+      DocumentSource.GENERATED,
+    ] as const),
+    type: faker.helpers.arrayElement([
+      DocumentType.INVOICE,
+      DocumentType.CLAIM_FORM,
+      DocumentType.REJECTION_LETTER,
+      DocumentType.GUARANTEE_LETTER,
+      DocumentType.RECAP,
+      DocumentType.EXPEDITION,
+      DocumentType.TRANSFER,
+    ] as const),
   };
 }
 export function fakeDocumentComplete() {
@@ -275,15 +393,32 @@ export function fakeDocumentComplete() {
     printCount: faker.datatype.number(),
     createdAt: new Date(),
     updatedAt: faker.datatype.datetime(),
-    source: faker.helpers.arrayElement([DocumentSource.UPLOADED, DocumentSource.GENERATED] as const),
-    type: faker.helpers.arrayElement([DocumentType.INVOICE, DocumentType.CLAIM_FORM, DocumentType.REJECTION_LETTER, DocumentType.GUARANTEE_LETTER, DocumentType.RECAP, DocumentType.EXPEDITION, DocumentType.TRANSFER] as const),
+    source: faker.helpers.arrayElement([
+      DocumentSource.UPLOADED,
+      DocumentSource.GENERATED,
+    ] as const),
+    type: faker.helpers.arrayElement([
+      DocumentType.INVOICE,
+      DocumentType.CLAIM_FORM,
+      DocumentType.REJECTION_LETTER,
+      DocumentType.GUARANTEE_LETTER,
+      DocumentType.RECAP,
+      DocumentType.EXPEDITION,
+      DocumentType.TRANSFER,
+    ] as const),
     claimProcessId: undefined,
   };
 }
 export function fakeTag() {
   return {
     name: faker.name.fullName(),
-    color: faker.helpers.arrayElement([Color.WHITE, Color.BLUE, Color.RED, Color.YELLOW, Color.GREEN] as const),
+    color: faker.helpers.arrayElement([
+      Color.WHITE,
+      Color.BLUE,
+      Color.RED,
+      Color.YELLOW,
+      Color.GREEN,
+    ] as const),
     updatedAt: faker.datatype.datetime(),
   };
 }
@@ -291,7 +426,13 @@ export function fakeTagComplete() {
   return {
     id: faker.datatype.number(),
     name: faker.name.fullName(),
-    color: faker.helpers.arrayElement([Color.WHITE, Color.BLUE, Color.RED, Color.YELLOW, Color.GREEN] as const),
+    color: faker.helpers.arrayElement([
+      Color.WHITE,
+      Color.BLUE,
+      Color.RED,
+      Color.YELLOW,
+      Color.GREEN,
+    ] as const),
     createdAt: new Date(),
     updatedAt: faker.datatype.datetime(),
   };
@@ -385,7 +526,17 @@ export function fakeClaimStatus() {
   return {
     description: undefined,
     rejectionReason: undefined,
-    status: faker.helpers.arrayElement([ClaimStatusType.DOCUMENT, ClaimStatusType.PENDING, ClaimStatusType.APPROVED, ClaimStatusType.REJECTED, ClaimStatusType.RELEASE, ClaimStatusType.PAID, ClaimStatusType.COMPLETED, ClaimStatusType.CANCELLED, ClaimStatusType.REOPEN] as const),
+    status: faker.helpers.arrayElement([
+      ClaimStatusType.DOCUMENT,
+      ClaimStatusType.PENDING,
+      ClaimStatusType.APPROVED,
+      ClaimStatusType.REJECTED,
+      ClaimStatusType.RELEASE,
+      ClaimStatusType.PAID,
+      ClaimStatusType.COMPLETED,
+      ClaimStatusType.CANCELLED,
+      ClaimStatusType.REOPEN,
+    ] as const),
   };
 }
 export function fakeClaimStatusComplete() {
@@ -394,7 +545,17 @@ export function fakeClaimStatusComplete() {
     description: undefined,
     rejectionReason: undefined,
     createdAt: new Date(),
-    status: faker.helpers.arrayElement([ClaimStatusType.DOCUMENT, ClaimStatusType.PENDING, ClaimStatusType.APPROVED, ClaimStatusType.REJECTED, ClaimStatusType.RELEASE, ClaimStatusType.PAID, ClaimStatusType.COMPLETED, ClaimStatusType.CANCELLED, ClaimStatusType.REOPEN] as const),
+    status: faker.helpers.arrayElement([
+      ClaimStatusType.DOCUMENT,
+      ClaimStatusType.PENDING,
+      ClaimStatusType.APPROVED,
+      ClaimStatusType.REJECTED,
+      ClaimStatusType.RELEASE,
+      ClaimStatusType.PAID,
+      ClaimStatusType.COMPLETED,
+      ClaimStatusType.CANCELLED,
+      ClaimStatusType.REOPEN,
+    ] as const),
     createById: faker.datatype.uuid(),
     claimId: faker.datatype.uuid(),
     rejectionLetterId: undefined,
