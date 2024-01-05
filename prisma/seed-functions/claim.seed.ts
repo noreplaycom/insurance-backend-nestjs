404364e01ea1409f106b3c5c8231caca6a03590e
Dikki Setiawan <initialsds@gmail.com>
2024-01-05 19:27:41 +0700

    updated tsconfig after a hour  dbugging for prisma intellisense anomalyal

--- prisma/seed-functions/claim.seed.ts
+++ prisma/seed-functions/claim.seed.ts
@@ -1,26 +1,35 @@
-import { AdmedicaStatus, ClaimChannel, Prisma, PrismaClient } from '@prisma/client';
+import { AdmedicaStatus, ClaimChannel, PrismaClient } from '@prisma/client'
 import { faker } from '@faker-js/faker';
 
-const prisma = new PrismaClient();
+const prisma = new PrismaClient()
 
 export async function claimCreateOne() {
+  const findParticipantId = await prisma.participant.findFirst({
+    skip: faker.datatype.number({ min: 1, max: 100 }),
+    select: { userId: true },
+  });
 
-  const query: Prisma.ClaimCreateArgs = {data: {company: }}
+  if (findParticipantId) {
+    console.log(
+      await prisma.claim.create({
+        data: {
+          channel: faker.helpers.arrayElement([
+            ClaimChannel.EMAIL,
+            ClaimChannel.REGULER,
+            ClaimChannel.WA,
+          ] as const),
+          admedicaStatus: faker.helpers.arrayElement([
+            AdmedicaStatus.NON_ADMEDICA,
+            AdmedicaStatus.ADMEDICA,
+          ] as const),
+          company: 'YAYASAN DANAR DANA SWADHARMA',
+          participantId: findParticipantId.userId,
+          cla
 
-  console.log(
-    await prisma.claim.create({
-      data: {
-        channel: faker.helpers.arrayElement([
-          ClaimChannel.EMAIL,
-          ClaimChannel.REGULER,
-          ClaimChannel.WA,
-        ] as const),
-        admedicaStatus: faker.helpers.arrayElement([
-          AdmedicaStatus.NON_ADMEDICA,
-          AdmedicaStatus.ADMEDICA,
-        ] as const),
-        company: { conn: { id: 1 } },
-      },
-    }),
-  );
+        },
+      }),
+    );
+  }
+
+
 }
