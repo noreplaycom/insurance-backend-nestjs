330c97d82f5b28e8e021db79b9f4096f163bb3c5
Dikki Setiawan <initialsds@gmail.com>
2024-01-10 16:16:34 +0700

    Add Branch and Region modules

--- src/services/branch/branch.resolver.ts
+++ src/services/branch/branch.resolver.ts
@@ -28,46 +28,46 @@ interface BranchSelect {
 export class BranchResolver {
   constructor(private readonly branchController: BranchController) {}
 
-  @Mutation(() => Branch, {
-    nullable: true,
-    description: 'Deskripsinya ada disini loh',
-  })
-  async branchCreateOne(
-    @Args()
-    branchCreateArgs: CreateOneBranchArgs,
-    @Relations() relations: BranchSelect,
-  ): Promise<Branch | void> {
-    return await this.branchController.createOne({
-      ...branchCreateArgs,
-      select: relations.select,
-    });
-  }
+  // @Mutation(() => Branch, {
+  //   nullable: true,
+  //   description: 'Deskripsinya ada disini loh',
+  // })
+  // async branchCreateOne(
+  //   @Args()
+  //   branchCreateArgs: CreateOneBranchArgs,
+  //   @Relations() relations: BranchSelect,
+  // ): Promise<Branch | void> {
+  //   return await this.branchController.createOne({
+  //     ...branchCreateArgs,
+  //     select: relations.select,
+  //   });
+  // }
 
-  @Mutation(() => BatchPayload, {
-    nullable: true,
-    description: 'Deskripsinya ada disini loh',
-  })
-  async branchCreateMany(
-    @Args()
-    createManyBranchArgs: CreateManyBranchArgs,
-  ) {
-    return await this.branchController.createMany(createManyBranchArgs);
-  }
+  // @Mutation(() => BatchPayload, {
+  //   nullable: true,
+  //   description: 'Deskripsinya ada disini loh',
+  // })
+  // async branchCreateMany(
+  //   @Args()
+  //   createManyBranchArgs: CreateManyBranchArgs,
+  // ) {
+  //   return await this.branchController.createMany(createManyBranchArgs);
+  // }
 
-  @Query(() => Branch, {
-    nullable: true,
-    description: 'Deskripsinya ada disini loh',
-  })
-  branchFindOne(
-    @Args()
-    branchFindUniqueArgs: FindUniqueBranchArgs,
-    @Relations() relations: BranchSelect,
-  ): Promise<Branch | void> {
-    return this.branchController.findOne({
-      ...branchFindUniqueArgs,
-      select: relations.select,
-    });
-  }
+  // @Query(() => Branch, {
+  //   nullable: true,
+  //   description: 'Deskripsinya ada disini loh',
+  // })
+  // branchFindOne(
+  //   @Args()
+  //   branchFindUniqueArgs: FindUniqueBranchArgs,
+  //   @Relations() relations: BranchSelect,
+  // ): Promise<Branch | void> {
+  //   return this.branchController.findOne({
+  //     ...branchFindUniqueArgs,
+  //     select: relations.select,
+  //   });
+  // }
 
   @Query(() => [Branch], {
     nullable: true,
@@ -83,78 +83,78 @@ export class BranchResolver {
     });
   }
 
-  @Query(() => Branch, {
-    nullable: true,
-    description: 'Deskripsinya ada disini loh',
-  })
-  branchFindFirst(
-    @Args()
-    findFirstBranchArgs: FindFirstBranchArgs,
-    @Relations() relations: BranchSelect,
-  ): Promise<Branch | void> {
-    return this.branchController.findFirst({
-      ...findFirstBranchArgs,
-      select: relations.select,
-    });
-  }
+  // @Query(() => Branch, {
+  //   nullable: true,
+  //   description: 'Deskripsinya ada disini loh',
+  // })
+  // branchFindFirst(
+  //   @Args()
+  //   findFirstBranchArgs: FindFirstBranchArgs,
+  //   @Relations() relations: BranchSelect,
+  // ): Promise<Branch | void> {
+  //   return this.branchController.findFirst({
+  //     ...findFirstBranchArgs,
+  //     select: relations.select,
+  //   });
+  // }
 
-  @Mutation(() => Branch, {
-    nullable: true,
-    description: 'Deskripsinya ada disini loh',
-  })
-  async branchUpdateOne(
-    @Args() branchUpdateOneArgs: UpdateOneBranchArgs,
-    @Relations() relations: BranchSelect,
-  ) {
-    return this.branchController.updateOne({
-      ...replaceNullWithUndefined(branchUpdateOneArgs),
-      select: relations.select,
-    });
-  }
+  // @Mutation(() => Branch, {
+  //   nullable: true,
+  //   description: 'Deskripsinya ada disini loh',
+  // })
+  // async branchUpdateOne(
+  //   @Args() branchUpdateOneArgs: UpdateOneBranchArgs,
+  //   @Relations() relations: BranchSelect,
+  // ) {
+  //   return this.branchController.updateOne({
+  //     ...replaceNullWithUndefined(branchUpdateOneArgs),
+  //     select: relations.select,
+  //   });
+  // }
 
-  @Mutation(() => Branch, {
-    nullable: true,
-    description: 'Deskripsinya ada disini loh',
-  })
-  async branchUpdateMany(@Args() updateManyBranchArgs: UpdateManyBranchArgs) {
-    return this.branchController.updateMany(updateManyBranchArgs);
-  }
+  // @Mutation(() => Branch, {
+  //   nullable: true,
+  //   description: 'Deskripsinya ada disini loh',
+  // })
+  // async branchUpdateMany(@Args() updateManyBranchArgs: UpdateManyBranchArgs) {
+  //   return this.branchController.updateMany(updateManyBranchArgs);
+  // }
 
-  @Mutation(() => Boolean, {
-    nullable: false,
-    description: 'Deskripsinya ada disini loh',
-  })
-  async branchDelete(
-    @Args() deleteOneBranchArgs: DeleteOneBranchArgs,
-    @Relations() relations: BranchSelect,
-  ) {
-    return this.branchController.delete({
-      ...deleteOneBranchArgs,
-      select: relations.select,
-    });
-  }
+  // @Mutation(() => Boolean, {
+  //   nullable: false,
+  //   description: 'Deskripsinya ada disini loh',
+  // })
+  // async branchDelete(
+  //   @Args() deleteOneBranchArgs: DeleteOneBranchArgs,
+  //   @Relations() relations: BranchSelect,
+  // ) {
+  //   return this.branchController.delete({
+  //     ...deleteOneBranchArgs,
+  //     select: relations.select,
+  //   });
+  // }
 
-  @Mutation(() => Boolean, {
-    nullable: false,
-    description: 'Deskripsinya ada disini loh',
-  })
-  async branchDeleteMany(@Args() deleteManyBranchArgs: DeleteManyBranchArgs) {
-    return this.branchController.deleteMany(deleteManyBranchArgs);
-  }
+  // @Mutation(() => Boolean, {
+  //   nullable: false,
+  //   description: 'Deskripsinya ada disini loh',
+  // })
+  // async branchDeleteMany(@Args() deleteManyBranchArgs: DeleteManyBranchArgs) {
+  //   return this.branchController.deleteMany(deleteManyBranchArgs);
+  // }
 
-  @Query(() => AggregateBranch, {
-    nullable: true,
-    description: 'Deskripsinya ada disini loh',
-  })
-  branchAggregate(@Args() branchAggregateArgs: BranchAggregateArgs) {
-    return this.branchController.aggregate(branchAggregateArgs);
-  }
+  // @Query(() => AggregateBranch, {
+  //   nullable: true,
+  //   description: 'Deskripsinya ada disini loh',
+  // })
+  // branchAggregate(@Args() branchAggregateArgs: BranchAggregateArgs) {
+  //   return this.branchController.aggregate(branchAggregateArgs);
+  // }
 
-  @Query(() => Float, {
-    nullable: true,
-    description: 'Deskripsinya ada disini loh',
-  })
-  branchCount(@Args() branchCountAggregateInput: FindManyBranchArgs) {
-    return this.branchController.count(branchCountAggregateInput);
-  }
+  // @Query(() => Float, {
+  //   nullable: true,
+  //   description: 'Deskripsinya ada disini loh',
+  // })
+  // branchCount(@Args() branchCountAggregateInput: FindManyBranchArgs) {
+  //   return this.branchController.count(branchCountAggregateInput);
+  // }
 }
--- src/services/group/group.resolver.ts
+++ src/services/group/group.resolver.ts
@@ -28,133 +28,133 @@ interface GroupSelect {
 export class GroupResolver {
   constructor(private readonly groupController: GroupController) {}
 
-  @Mutation(() => Group, {
-    nullable: true,
-    description: 'Deskripsinya ada disini loh',
-  })
-  async groupCreateOne(
-    @Args()
-    groupCreateArgs: CreateOneGroupArgs,
-    @Relations() relations: GroupSelect,
-  ): Promise<Group | void> {
-    return await this.groupController.createOne({
-      ...groupCreateArgs,
-      select: relations.select,
-    });
-  }
+  // @Mutation(() => Group, {
+  //   nullable: true,
+  //   description: 'Deskripsinya ada disini loh',
+  // })
+  // async groupCreateOne(
+  //   @Args()
+  //   groupCreateArgs: CreateOneGroupArgs,
+  //   @Relations() relations: GroupSelect,
+  // ): Promise<Group | void> {
+  //   return await this.groupController.createOne({
+  //     ...groupCreateArgs,
+  //     select: relations.select,
+  //   });
+  // }
 
-  @Mutation(() => BatchPayload, {
-    nullable: true,
-    description: 'Deskripsinya ada disini loh',
-  })
-  async groupCreateMany(
-    @Args()
-    createManyGroupArgs: CreateManyGroupArgs,
-  ) {
-    return await this.groupController.createMany(createManyGroupArgs);
-  }
+  // @Mutation(() => BatchPayload, {
+  //   nullable: true,
+  //   description: 'Deskripsinya ada disini loh',
+  // })
+  // async groupCreateMany(
+  //   @Args()
+  //   createManyGroupArgs: CreateManyGroupArgs,
+  // ) {
+  //   return await this.groupController.createMany(createManyGroupArgs);
+  // }
 
-  @Query(() => Group, {
-    nullable: true,
-    description: 'Deskripsinya ada disini loh',
-  })
-  groupFindOne(
-    @Args()
-    groupFindUniqueArgs: FindUniqueGroupArgs,
-    @Relations() relations: GroupSelect,
-  ): Promise<Group | void> {
-    return this.groupController.findOne({
-      ...groupFindUniqueArgs,
-      select: relations.select,
-    });
-  }
+  // @Query(() => Group, {
+  //   nullable: true,
+  //   description: 'Deskripsinya ada disini loh',
+  // })
+  // groupFindOne(
+  //   @Args()
+  //   groupFindUniqueArgs: FindUniqueGroupArgs,
+  //   @Relations() relations: GroupSelect,
+  // ): Promise<Group | void> {
+  //   return this.groupController.findOne({
+  //     ...groupFindUniqueArgs,
+  //     select: relations.select,
+  //   });
+  // }
 
-  @Query(() => [Group], {
-    nullable: true,
-    description: 'Deskripsinya ada disini loh',
-  })
-  groupFindMany(
-    @Args() groupFindManyArgs: FindManyGroupArgs,
-    @Relations() relations: GroupSelect,
-  ) {
-    return this.groupController.findMany({
-      ...groupFindManyArgs,
-      select: relations.select,
-    });
-  }
+  // @Query(() => [Group], {
+  //   nullable: true,
+  //   description: 'Deskripsinya ada disini loh',
+  // })
+  // groupFindMany(
+  //   @Args() groupFindManyArgs: FindManyGroupArgs,
+  //   @Relations() relations: GroupSelect,
+  // ) {
+  //   return this.groupController.findMany({
+  //     ...groupFindManyArgs,
+  //     select: relations.select,
+  //   });
+  // }
 
-  @Query(() => Group, {
-    nullable: true,
-    description: 'Deskripsinya ada disini loh',
-  })
-  groupFindFirst(
-    @Args()
-    findFirstGroupArgs: FindFirstGroupArgs,
-    @Relations() relations: GroupSelect,
-  ): Promise<Group | void> {
-    return this.groupController.findFirst({
-      ...findFirstGroupArgs,
-      select: relations.select,
-    });
-  }
+  // @Query(() => Group, {
+  //   nullable: true,
+  //   description: 'Deskripsinya ada disini loh',
+  // })
+  // groupFindFirst(
+  //   @Args()
+  //   findFirstGroupArgs: FindFirstGroupArgs,
+  //   @Relations() relations: GroupSelect,
+  // ): Promise<Group | void> {
+  //   return this.groupController.findFirst({
+  //     ...findFirstGroupArgs,
+  //     select: relations.select,
+  //   });
+  // }
 
-  @Mutation(() => Group, {
-    nullable: true,
-    description: 'Deskripsinya ada disini loh',
-  })
-  async groupUpdateOne(
-    @Args() groupUpdateOneArgs: UpdateOneGroupArgs,
-    @Relations() relations: GroupSelect,
-  ) {
-    return this.groupController.updateOne({
-      ...replaceNullWithUndefined(groupUpdateOneArgs),
-      select: relations.select,
-    });
-  }
+  // @Mutation(() => Group, {
+  //   nullable: true,
+  //   description: 'Deskripsinya ada disini loh',
+  // })
+  // async groupUpdateOne(
+  //   @Args() groupUpdateOneArgs: UpdateOneGroupArgs,
+  //   @Relations() relations: GroupSelect,
+  // ) {
+  //   return this.groupController.updateOne({
+  //     ...replaceNullWithUndefined(groupUpdateOneArgs),
+  //     select: relations.select,
+  //   });
+  // }
 
-  @Mutation(() => Group, {
-    nullable: true,
-    description: 'Deskripsinya ada disini loh',
-  })
-  async groupUpdateMany(@Args() updateManyGroupArgs: UpdateManyGroupArgs) {
-    return this.groupController.updateMany(updateManyGroupArgs);
-  }
+  // @Mutation(() => Group, {
+  //   nullable: true,
+  //   description: 'Deskripsinya ada disini loh',
+  // })
+  // async groupUpdateMany(@Args() updateManyGroupArgs: UpdateManyGroupArgs) {
+  //   return this.groupController.updateMany(updateManyGroupArgs);
+  // }
 
-  @Mutation(() => Boolean, {
-    nullable: false,
-    description: 'Deskripsinya ada disini loh',
-  })
-  async groupDelete(
-    @Args() deleteOneGroupArgs: DeleteOneGroupArgs,
-    @Relations() relations: GroupSelect,
-  ) {
-    return this.groupController.delete({
-      ...deleteOneGroupArgs,
-      select: relations.select,
-    });
-  }
+  // @Mutation(() => Boolean, {
+  //   nullable: false,
+  //   description: 'Deskripsinya ada disini loh',
+  // })
+  // async groupDelete(
+  //   @Args() deleteOneGroupArgs: DeleteOneGroupArgs,
+  //   @Relations() relations: GroupSelect,
+  // ) {
+  //   return this.groupController.delete({
+  //     ...deleteOneGroupArgs,
+  //     select: relations.select,
+  //   });
+  // }
 
-  @Mutation(() => Boolean, {
-    nullable: false,
-    description: 'Deskripsinya ada disini loh',
-  })
-  async groupDeleteMany(@Args() deleteManyGroupArgs: DeleteManyGroupArgs) {
-    return this.groupController.deleteMany(deleteManyGroupArgs);
-  }
+  // @Mutation(() => Boolean, {
+  //   nullable: false,
+  //   description: 'Deskripsinya ada disini loh',
+  // })
+  // async groupDeleteMany(@Args() deleteManyGroupArgs: DeleteManyGroupArgs) {
+  //   return this.groupController.deleteMany(deleteManyGroupArgs);
+  // }
 
-  @Query(() => AggregateGroup, {
-    nullable: true,
-    description: 'Deskripsinya ada disini loh',
-  })
-  groupAggregate(@Args() groupAggregateArgs: GroupAggregateArgs) {
-    return this.groupController.aggregate(groupAggregateArgs);
-  }
+  // @Query(() => AggregateGroup, {
+  //   nullable: true,
+  //   description: 'Deskripsinya ada disini loh',
+  // })
+  // groupAggregate(@Args() groupAggregateArgs: GroupAggregateArgs) {
+  //   return this.groupController.aggregate(groupAggregateArgs);
+  // }
 
-  @Query(() => Float, {
-    nullable: true,
-    description: 'Deskripsinya ada disini loh',
-  })
-  groupCount(@Args() groupCountAggregateInput: FindManyGroupArgs) {
-    return this.groupController.count(groupCountAggregateInput);
-  }
+  // @Query(() => Float, {
+  //   nullable: true,
+  //   description: 'Deskripsinya ada disini loh',
+  // })
+  // groupCount(@Args() groupCountAggregateInput: FindManyGroupArgs) {
+  //   return this.groupController.count(groupCountAggregateInput);
+  // }
 }
